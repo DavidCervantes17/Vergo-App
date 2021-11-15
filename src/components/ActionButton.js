ec7bc57014abf63e9d2908b784e3e3a7a4553bda
David Cervantes <davs.cear98@gmail.com>
2021-11-15 16:50:53 -0600

    Action buttons

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
-export default function ActionButton() {
+export default function ActionButton(props) {
+    const {title="Send", icon=faPaperPlane} = props
     return (
         <TouchableOpacity style={styles.container}>
-            
+            <View style={styles.icon}>
+            <View>
+              <FontAwesomeIcon size={ 26 }  icon={ icon } />
+            </View>
+            </View>
+            <View style={styles.text}>
+              <Text>{title}</Text>
+            </View>
+
         </TouchableOpacity>
     )
 }
@@ -20,7 +30,21 @@ const styles = StyleSheet.create({
       shadowOpacity: 0.5,
       shadowRadius: 2,
       elevation: 2,
-      marginLeft:24
+      marginLeft:24,
+      flexDirection: 'row',
     },
+    icon: {
+      alignItems: 'center',
+      alignSelf: 'center',
+      flex:2,
+    },
+    text: {
+      alignItems: 'center',
+      alignSelf: 'center',
+      flex:3,
+      marginRight:8,
+      fontWeight:"bold"
+      
+    }
   });
   
--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import ActionButton from './ActionButton'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
+import { faChartPie } from '@fortawesome/free-solid-svg-icons'
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons'
 
 export default function ActionButtons() {
     return (
         <ScrollView showsHorizontalScrollIndicator={false}
         horizontal={true}>
-            <ActionButton/>
-            <ActionButton/>
-            <ActionButton/>
-            <ActionButton/>
-            <ActionButton/>
+            <ActionButton title={"Send Payment"} icon={faPaperPlane}/>
+            <ActionButton title={"Pay Card"} icon={faDollarSign}/>
+            <ActionButton title={"Analytics"} icon={faChartPie}/>
+            <ActionButton title={"Freeze Card"} icon={faSnowflake}/>
         </ScrollView>
     )
 }
