c3c27e2942d2e35a9d2764c89fbc8ff2f55ace03
David Cervantes <davs.cear98@gmail.com>
2021-11-15 23:12:55 -0600

    Scroll cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,81 +1,80 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native';
-import cardNumberFormat from '../utilities/cardNumberFormat'
-import numberFormat from '../utilities/numberFormat'
+import React from "react";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import cardNumberFormat from "../utilities/cardNumberFormat";
+import numberFormat from "../utilities/numberFormat";
+const windowWidth = Dimensions.get("window").width;
 
 export default function Card(props) {
-    const { accountName="Debit Card", amount="1000000.79", color="#a0a0a0", type="Balance" } = props;
+  const {
+    accountName = "Debit Card",
+    amount = "1000000.79",
+    color = "#a0a0a0",
+    type = "Balance",
+  } = props;
 
-    return (
-        <View style={[styles.container, {backgroundColor:color}]}>
-            <View style={styles.card}>
-                <View style={styles.containerUp}>
-                    <Text  style={styles.textAccountName}>
-                        {accountName}
-                    </Text>
-                    <Text  style={styles.textType}>
-                        {type}
-                    </Text>
-                    <Text  style={styles.textBalance}>
-                        {numberFormat(amount)}
-                    </Text>
-                </View>
-                <View style={styles.containerDown}>
-                    <Image style={styles.image} source={require('../img/visa-white.png')}/>
-                </View>
-
-            </View>
+  return (
+    <View style={[styles.container, { backgroundColor: color }]}>
+      <View style={styles.card}>
+        <View style={styles.containerUp}>
+          <Text style={styles.textAccountName}>{accountName}</Text>
+          <Text style={styles.textType}>{type}</Text>
+          <Text style={styles.textBalance}>{numberFormat(amount)}</Text>
         </View>
-
-    )
+        <View style={styles.containerDown}>
+          <Image
+            style={styles.image}
+            source={require("../img/visa-white.png")}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding:24,
-        marginLeft: 36,
-        width: 300,
-        height:170,
-        maxHeight: 170,
-        borderRadius: 10
-    },
-    card: {
-        flex:1
-    },
-    textNumber:{
-        color: '#000',
-    },
-    textAccountName:{
-        color: '#000',
-    },
-    textBalance:{
-        marginTop:0,
-        fontSize: 24,
-        color: '#000',
-        fontWeight: 'bold',
-    },
-    image:{
-        marginTop:24,
-        marginLeft:12,
-        width:45,
-        height:14,
-        alignItems: 'flex-end'
-
-    },
-    containerUp:{
-        flex:1,
-    },
-    containerDown:{
-        flex:1,
-        alignSelf: 'flex-end',
-        alignItems: 'flex-end',
-        flexDirection: 'row', justifyContent: 'flex-end',
-    },
-    textType: {
-        color: '#000',
-        fontSize: 12,
-        marginTop:12
-    }
-
-    
-  });
+  container: {
+    padding: 24,
+    marginHorizontal: (windowWidth - 300) / 2,
+    width: 300,
+    height: 170,
+    maxHeight: 170,
+    borderRadius: 10,
+  },
+  card: {
+    flex: 1,
+  },
+  textNumber: {
+    color: "#000",
+  },
+  textAccountName: {
+    color: "#000",
+  },
+  textBalance: {
+    marginTop: 0,
+    fontSize: 24,
+    color: "#000",
+    fontWeight: "bold",
+  },
+  image: {
+    marginTop: 24,
+    marginLeft: 12,
+    width: 45,
+    height: 14,
+    alignItems: "flex-end",
+  },
+  containerUp: {
+    flex: 1,
+  },
+  containerDown: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "flex-end",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  textType: {
+    color: "#000",
+    fontSize: 12,
+    marginTop: 12,
+  },
+});
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
 import Card from "./Card";
 
 export default function CardList() {
+    const windowWidth = Dimensions.get("window").width;
 
     const handleScroll = (event) => {
         console.log(event.nativeEvent.contentOffset.x);
 
     }
     return (
-        <ScrollView  onScroll={handleScroll} scrollEventThrottle={16} style={styles.container} horizontal={true} showsHorizontalScrollIndicator={false}>
+        <ScrollView  
+        onScroll={handleScroll} scrollEventThrottle={16} style={styles.container} 
+        horizontal={true} showsHorizontalScrollIndicator={false}
+                    disableIntervalMomentum={true}
+                    snapToInterval={windowWidth}
+                  
+                    decelerationRate={"fast"}
+                    pagingEnabled={true}
+        
+        >
             <Card color="#7fc4b4" amount="109873.98" accountName="Debit Card" type="Balance"/>
             <Card color="#b6daee" amount="64.98"  accountName="Credit Card" type="Used Balance"/>
             <Card color="#fa9984" amount="100.98"  accountName="Joe's Project" type="Used Balance"/>
