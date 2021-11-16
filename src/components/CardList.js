6e1527926814906c6bd61d6a5c9febe428dd2477
David Cervantes <davs.cear98@gmail.com>
2021-11-15 23:13:20 -0600

    Scroll cards

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,36 +1,49 @@
-import React from 'react'
+import React from "react";
 import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
 import Card from "./Card";
 
 export default function CardList() {
-    const windowWidth = Dimensions.get("window").width;
+  const windowWidth = Dimensions.get("window").width;
 
-    const handleScroll = (event) => {
-        console.log(event.nativeEvent.contentOffset.x);
-
-    }
-    return (
-        <ScrollView  
-        onScroll={handleScroll} scrollEventThrottle={16} style={styles.container} 
-        horizontal={true} showsHorizontalScrollIndicator={false}
-                    disableIntervalMomentum={true}
-                    snapToInterval={windowWidth}
-                  
-                    decelerationRate={"fast"}
-                    pagingEnabled={true}
-        
-        >
-            <Card color="#7fc4b4" amount="109873.98" accountName="Debit Card" type="Balance"/>
-            <Card color="#b6daee" amount="64.98"  accountName="Credit Card" type="Used Balance"/>
-            <Card color="#fa9984" amount="100.98"  accountName="Joe's Project" type="Used Balance"/>
-        </ScrollView>
-    )
+  const handleScroll = (event) => {
+    console.log(event.nativeEvent.contentOffset.x);
+  };
+  return (
+    <ScrollView
+      onScroll={handleScroll}
+      scrollEventThrottle={16}
+      style={styles.container}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      disableIntervalMomentum={true}
+      snapToInterval={windowWidth}
+      decelerationRate={"fast"}
+      pagingEnabled={true}
+    >
+      <Card
+        color="#7fc4b4"
+        amount="109873.98"
+        accountName="Debit Card"
+        type="Balance"
+      />
+      <Card
+        color="#b6daee"
+        amount="64.98"
+        accountName="Credit Card"
+        type="Used Balance"
+      />
+      <Card
+        color="#fa9984"
+        amount="100.98"
+        accountName="Joe's Project"
+        type="Used Balance"
+      />
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      maxHeight:200,
-    },
-    
-  });
-  
+  container: {
+    maxHeight: 200,
+  },
+});
